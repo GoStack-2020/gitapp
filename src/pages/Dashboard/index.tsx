76799906864722c0398f31fe078dd7de7343470a
Indiara Duarte <indiara96d@gmail.com>
2020-09-11 23:59:20 -0300

    Styling dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,6 +1,37 @@
 /* eslint-disable no-use-before-define */
 import React from 'react';
-import { Title } from './styles';
+import { FiChevronRight } from 'react-icons/fi';
+import { Title, Form, Repositories } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+
+// eslint-disable-next-line arrow-body-style
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars3.githubusercontent.com/u/15816672?s=460&u=71fca47aae76a640d7b38ac1dc1e9515021d7205&v=4"
+            alt="Indiara Duarte"
+          />
+          <div>
+            <strong>rocketseat</strong>
+            <p>jdnasjdnsajkdnsajdn</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
+    </>
+  );
+};
 export default Dashboard;
