c0665f8b02bcf891227a8932e115bcef615dfd85
Indiara Duarte <indiara96d@gmail.com>
2020-09-12 03:16:26 -0300

    Connecting git api to front components

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,35 +1,65 @@
-/* eslint-disable no-use-before-define */
-import React from 'react';
+// eslint-disable-next-line no-use-before-define
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 import { Title, Form, Repositories } from './styles';
 import logoImg from '../../assets/logo.svg';
+import api from '../../services/api';
 
+interface Repository {
+  // eslint-disable-next-line camelcase
+  full_name: string;
+  owner: {
+    login: string;
+    // eslint-disable-next-line camelcase
+    avatar_url: string;
+    description: string;
+  };
+}
 
 // eslint-disable-next-line arrow-body-style
 const Dashboard: React.FC = () => {
+  const [newRepo, setNewRepo] = useState('');
+  // eslint-disable-next-line
+  const [repositories, setRepositories] = useState<Repository[]>([]);
+
+  async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    const response = await api.get(`repos/${newRepo}`);
+
+    const repository = response.data;
+
+    setRepositories([...repositories, repository]);
+    setNewRepo('');
+  }
+
   return (
     <>
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form>
-        <input placeholder="Digite o nome do repositório" />
+      <Form onSubmit={handleAddRepository}>
+        <input
+          value={newRepo}
+          placeholder="Digite o nome do repositório"
+          onChange={(e) => setNewRepo(e.target.value)}
+        />
         <button type="submit">Pesquisar</button>
       </Form>
 
       <Repositories>
-        <a href="teste">
-          <img
-            src="https://avatars3.githubusercontent.com/u/15816672?s=460&u=71fca47aae76a640d7b38ac1dc1e9515021d7205&v=4"
-            alt="Indiara Duarte"
-          />
-          <div>
-            <strong>rocketseat</strong>
-            <p>jdnasjdnsajkdnsajdn</p>
-          </div>
-
-          <FiChevronRight size={20} />
-        </a>
+        {repositories.map((repository) => (
+          <a key={repository.full_name} href="teste">
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.owner.description}</p>
+            </div>
+            <FiChevronRight size={20} />
+          </a>
+        ))}
       </Repositories>
     </>
   );
