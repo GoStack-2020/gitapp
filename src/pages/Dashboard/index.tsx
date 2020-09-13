f8285811483c80e876fa35a5659d41e917437794
Indiara Duarte <indiara96d@gmail.com>
2020-09-12 21:26:57 -0300

    Adding error handling to form component

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,12 @@
 // eslint-disable-next-line no-use-before-define
 import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
-import { Title, Form, Repositories } from './styles';
+import {
+  Title,
+  Form,
+  Repositories,
+  Error,
+} from './styles';
 import logoImg from '../../assets/logo.svg';
 import api from '../../services/api';
 
@@ -19,17 +24,28 @@ interface Repository {
 // eslint-disable-next-line arrow-body-style
 const Dashboard: React.FC = () => {
   const [newRepo, setNewRepo] = useState('');
+  const [inputError, setInputError] = useState('');
   // eslint-disable-next-line
   const [repositories, setRepositories] = useState<Repository[]>([]);
 
   async function handleAddRepository(event: FormEvent<HTMLFormElement>): Promise<void> {
     event.preventDefault();
-    const response = await api.get(`repos/${newRepo}`);
+    if (!newRepo) {
+      setInputError('Digite autor/nome do repositório');
+      return;
+    }
 
-    const repository = response.data;
+    try {
+      const response = await api.get(`repos/${newRepo}`);
 
-    setRepositories([...repositories, repository]);
-    setNewRepo('');
+      const repository = response.data;
+
+      setRepositories([...repositories, repository]);
+      setNewRepo('');
+      setInputError('');
+    } catch (err) {
+      setInputError('Erro na busca desse repositório');
+    }
   }
 
   return (
@@ -37,7 +53,7 @@ const Dashboard: React.FC = () => {
       <img src={logoImg} alt="Github Explorer" />
       <Title>Explore repositórios no Github</Title>
 
-      <Form onSubmit={handleAddRepository}>
+      <Form hasError={!!inputError} onSubmit={handleAddRepository}>
         <input
           value={newRepo}
           placeholder="Digite o nome do repositório"
@@ -45,6 +61,8 @@ const Dashboard: React.FC = () => {
         />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      { inputError && <Error>{inputError}</Error>}
 
       <Repositories>
         {repositories.map((repository) => (
